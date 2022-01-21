b1df39fbe25f585e121edb3aff027f136ebdb6ee
BASUDEV ROUT <57030150+basu021@users.noreply.github.com>
2022-01-21 21:14:21 +0530

    vote

---
+++ class/Day-5/vote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age=2022;
+
+    if(age >= 18 && age <= 120) {
+        printf("You are eligible");
+    }
+    else {
+        printf("You are not");
+    }
+}
