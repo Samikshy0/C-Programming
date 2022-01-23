cd3e9846debf326ac9977814ef1a2a464d2a2292
BASUDEV ROUT <57030150+basu021@users.noreply.github.com>
2022-01-23 21:11:48 +0530

    Create parameters.c

---
+++ class/Day-7/parameters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int add(int g, int h) {
+    return g+h;
+}
+
+int sub(int r,int u) {
+    return r-u;
+}
+
+int main() {
+
+    printf("%d", add(4,50));
+    printf("\n%d", sub(70,60));
+}
