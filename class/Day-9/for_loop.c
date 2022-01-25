35067756f1a92fd5dd21520acaf534384f320f64
BASUDEV ROUT <57030150+basu021@users.noreply.github.com>
2022-01-25 21:11:20 +0530

    Create for_loop.c

---
+++ class/Day-9/for_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    // for(initialization; condition; increment/decrement) {
+    //     // code to be executed
+    // }
+
+    // a++
+    // a = a+1
+    // a += 1
+    int d;
+    printf("Enter the value: ");
+    scanf("%d",&d);
+
+    for(int a=0; a<=d; a++) {
+        printf("%d\n",a);
+    }
+    
+}
