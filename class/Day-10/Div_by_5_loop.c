fea91923325c4e45b364f6c93c6dd076ac7808f0
BASUDEV ROUT <57030150+basu021@users.noreply.github.com>
2022-01-28 21:43:35 +0530

    Create Div_by_5_loop.c

---
+++ class/Day-10/Div_by_5_loop.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    
+    // i = i+1
+
+    // for(int i=1; i<=100; i=i+1) {
+    //     if(i%5==0)
+    //     printf("%d\n",i);
+    // }
+
+    // for(int i=5; i<=100; i = i+5) 
+    // {
+    //     printf("%d\n",i);
+    // }
+
+    // for(int i=1; i<=10; i++) {
+    //     printf("%d\n",i*5);
+    // }
+
+    // int i=1;
+    // while(i <= 100) {
+    //     if(i%5==0)
+    //     printf("%d\n",i);
+    //     i++;
+    // }
+
+    int i=5;
+    while(i <= 100) {
+        printf("%d\n",i);
+        i=i+5;
+    }
+    
+}
