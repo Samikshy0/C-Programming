044f284a680d7e1210a0a49f11068207bee80e28
BASUDEV ROUT <57030150+basu021@users.noreply.github.com>
2022-01-31 21:44:28 +0530

    Create fun_call_in_fun.c

---
+++ class/Day-13/fun_call_in_fun.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void msg();
+void name();
+
+int main(){
+msg();
+return 0;
+}
+
+void msg(){
+    printf("Hello Good evening !!\n");
+    name();
+}
+
+void name(){
+    printf("Welcome imran\n");
+}
