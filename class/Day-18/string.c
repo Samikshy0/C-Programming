d37d438209a7ac656032da4723140b45310bfa3b
basu021 <57030150+basu021@users.noreply.github.com>
2022-02-09 20:51:33 +0530

    string

---
+++ class/Day-18/string.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // char msg[200] = "hdfkjhsdf hdsfjhsdkafh";
+    // printf("%s",msg);
+    char h[10];
+    printf("Enter your name: ");
+    scanf("%s",h);
+    printf("Your name is %s\n",h);
+}
