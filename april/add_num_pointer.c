6fd299e95b7157b8d319313d5ede739eddb51479
basu021 <57030150+basu021@users.noreply.github.com>
2022-04-18 22:55:25 +0530

    .

--- april/add_num_pointer.c
+++ april/add_num_pointer.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int cv(int p) {
+    p = p + 10;
+}
+
 int main() {
-    
+    int a=6, b=7;
+
+    printf("Value of a: %d\n", a);
+    printf("Value of b: %d\n", b);
+
+    printf("calling the function cv\n");
+    cv(a);
+    printf("Value of a: %d\n", a);
+
 }
