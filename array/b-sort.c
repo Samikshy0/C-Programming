1ad83add54f60ca41caa087f247a0a69d2fdc47d
BASUDEV ROUT <basudevrout2001@gmail.com>
2022-05-19 19:46:56 +0530

    Update b-sort.c

--- array/b-sort.c
+++ array/b-sort.c
@@ -9,17 +9,19 @@ int swap(int *a, int *b)
 int main() {
     int arr[] = {1,7,4,8,2};
     // print the array
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%d ", arr[i]);
+    // }
+    // printf("\n");
     // sorting the array
     for (int i = 0; i < 5; i++)
     {
-        for(int j=0; j<5;j++) {
-            if(arr[i]<arr[j]) {
-                swap(&arr[i], &arr[j]);
+        for (int j = 0; j < 5 - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
             }
         }
     }
